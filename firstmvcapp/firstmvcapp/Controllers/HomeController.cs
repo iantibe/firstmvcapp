0765fec35e515e41100cf133874c5410e98f53cf
Ian Tibe <imtibe@dmacc.edu>
2019-02-06 21:52:35 -0600

    Final commit

---
+++ firstmvcapp/firstmvcapp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstmvcapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AboutUs
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        //GET: AboutGame
+        public ActionResult AboutGame()
+        {
+            return View();
+        }
+       
+    }
+}
